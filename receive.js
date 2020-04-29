ac7165d39c7839e618321738171efb6e92ec7a83
Scottie Enriquez <scott.c.enriquez@gmail.com>
2020-04-29 14:34:34 -0500

    Cleaning up code

--- receive.js
+++ receive.js
@@ -1,12 +1,8 @@
 const AWS = require('aws-sdk');
-
-// Set the region
 AWS.config.update({region: 'us-east-1'});
-
-// Create an SQS service object
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 const queueURL = 'https://sqs.us-east-1.amazonaws.com/201038669559/sa-cert-training';
-const params = {
+const parameters = {
     AttributeNames: [
         'SentTimestamp'
     ],
@@ -18,21 +14,22 @@ const params = {
     VisibilityTimeout: 30,
     WaitTimeSeconds: 0
 };
-sqs.receiveMessage(params, function(err, data) {
-    if (err) {
-        console.log("Receive Error", err);
+sqs.receiveMessage(parameters, (error, data) => {
+    if (error) {
+        console.log('Receive Error', error);
     }
     else if (data.Messages) {
         console.log(data.Messages);
-        const deleteParams = {
+        const deleteParameters = {
             QueueUrl: queueURL,
             ReceiptHandle: data.Messages[0].ReceiptHandle
         };
-        sqs.deleteMessage(deleteParams, function(err, data) {
-            if (err) {
-                console.log("Delete Error", err);
-            } else {
-                console.log("Message Deleted", data);
+        sqs.deleteMessage(deleteParameters, (error, data) => {
+            if (error) {
+                console.log('Delete Error', error);
+            }
+            else {
+                console.log('Message Deleted', data);
             }
         });
     }
